473dd01c912043f46e33b6e3e45c0a88b3cb891a
yehua.zyh <yehua.zyh@alibaba-inc.com>
2017-11-22 16:37:09 +0800

    testadd

--- com/ng/test/threadtest/QueueTest.py
+++ com/ng/test/threadtest/QueueTest.py
@@ -6,27 +6,31 @@ import time
 
 exitFlag = 0
 
-class myThread (threading.Thread):
+
+class myThread(threading.Thread):
     def __init__(self, threadID, name, q):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.q = q
+
     def run(self):
-        print ("开启线程：" + self.name)
+        print("开启线程：" + self.name)
         process_data(self.name, self.q)
-        print ("退出线程：" + self.name)
+        print("退出线程：" + self.name)
+
 
 def process_data(threadName, q):
     while not exitFlag:
         queueLock.acquire()
-        if not workQueue.empty():
+        while not workQueue.empty():
             data = q.get()
-            queueLock.release()
-            print ("%s processing %s" % (threadName, data))
-        else:
-            queueLock.release()
+            print("%s processing %s" % (threadName, data))
+
+        queueLock.release()
+
         time.sleep(1)
+
 
 threadList = ["Thread-1", "Thread-2", "Thread-3"]
 nameList = ["One", "Two", "Three", "Four", "Five"]
@@ -58,4 +62,4 @@ exitFlag = 1
 # 等待所有线程完成
 for t in threads:
     t.join()
-print ("退出主线程")
+print("退出主线程")
