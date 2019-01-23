5e13c6610e8e720928247d1ca6b3bb07bba37108
yhzhang <git12345678>
2019-01-23 14:09:44 +0800

    自动部署脚本

--- com/ng/test/inputoutput/url_test.py
+++ com/ng/test/inputoutput/url_test.py
@@ -3,6 +3,8 @@ import time
 from urllib import request
 from bs4 import BeautifulSoup
 
+oldjarmap = {}
+
 
 # 读取文件，读取以行为单位，每一行是列表里的一个元素
 def readJarsLink(url, path, runjar):
@@ -12,7 +14,7 @@ def readJarsLink(url, path, runjar):
 
     links = bsObj.findAll("a")
     for link in links:
-        if ('jar' == link.string[-3:]):
+        if 'jar' == link.string[-3:]:
             jarLinks.append(link['href'])
     jarLinks.sort()
 
@@ -22,6 +24,10 @@ def readJarsLink(url, path, runjar):
     os.chdir(path)
     ##下载文件
     jarUrl = jarLinks[-1]
+    if runjar in oldjarmap and jarname == oldjarmap[runjar]:
+        print('jar包未改动不需要重新build' + jarname)
+        return
+    oldjarmap[runjar] = jarname
     os.system("wget -q " + jarUrl)
     print(' 下载jar包:' + jarUrl)
 
@@ -38,12 +44,13 @@ def startJvm(runjar, newjar):
 ## 提取jar链接
 while True:
     ticks = time.time()
-    print('开始自动部署：'+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    print('开始自动部署：' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     readJarsLink(
         "http://192.168.20.6:8081/nexus/content/repositories/snapshots/com/orko/distribution/distribution-admin-web/0.0.1-SNAPSHOT/",
         '/home/www/app/distribution-admin', 'distribution-admin-web-0.0.1-SNAPSHOT.jar');
     readJarsLink(
         "http://192.168.20.6:8081/nexus/content/repositories/snapshots/com/orko/distribution/distribution-api/0.0.1-SNAPSHOT/",
         '/home/www/app/distribution-api', 'distribution-api-0.0.1-SNAPSHOT.jar');
-    print('完成自动部署：'+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    print('完成自动部署：' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    print(oldjarmap)
     time.sleep(300)
---
+++ com/ng/test/testdict.py
@@ -0,0 +1,12 @@
+dict={}
+
+print(type(dict))
+
+key='123'
+
+dict[key]='111111'
+
+print(dict[key])
+
+if(key in dict and dict[key]=='111111'):
+    print(123)
